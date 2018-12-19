7944c99b6fb5418e5ab2635dbed1132d954a084c
Nils Bokermann <nils.bokermann@bermuda.de>
2018-12-19 11:18:42 +0100

    Some debugging in sonar.sh

--- .github/main.workflow
+++ .github/main.workflow
@@ -1,4 +1,4 @@
-workflow "New workflow" {
+workflow "Maven Build and Check" {
   on = "push"
   resolves = ["Quality Check"]
 }
--- sonar-Action/sonar.sh
+++ sonar-Action/sonar.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
+set -e
 
 SONAR_HOST=http://development.bermuda.de/sonar
 export TASKID=$(mvn sonar:sonar \
      -Dsonar.host.url=${SONAR_HOST} \
      -Dsonar.login=${SONAR_TOKEN} | tee | grep task?id= | sed -e's/.*task?id=\(.*\)/\1/')
 
-export RESULT=$(/sonarqube-result.py --token=${SONAR_TOKEN} --Base=${SONAR_HOST}  --task=$TASKID)
+export RESULT=$(/sonarqube-result.py --token=${SONAR_TOKEN} --Base=${SONAR_HOST}  --task=${TASKID})
 if [ x$RESULT != xSUCCESS ]; then
     exit 1
 fi
