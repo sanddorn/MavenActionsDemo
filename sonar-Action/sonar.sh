de2ad05acacd55925431c9e0dc78d553aa9e45b0
Nils Bokermann <nils.bokermann@bermuda.de>
2018-12-19 16:57:13 +0100

    trying with the action id

--- sonar-Action/sonar.sh
+++ sonar-Action/sonar.sh
@@ -5,7 +5,12 @@ export TASKID=$(mvn sonar:sonar \
      -Dsonar.host.url=${SONAR_HOST} \
      -Dsonar.login=${SONAR_TOKEN} | tee | grep task?id= | sed -e's/.*task?id=\(.*\)/\1/')
 
-export RESULT=$(/sonarqube-result.py --token=${SONAR_TOKEN} --Base=${SONAR_HOST}  --task=${TASKID})
+echo "Task: ${TASKID}"
+
+export RESULT=$(/sonarqube-result.py --token=${SONAR_TOKEN} --Base=${SONAR_HOST}  --task="${TASKID}")
+
+echo "Result: ${RESULT}"
+
 if [ x$RESULT != xOK ]; then
     echo "RESULT: $RESULT"
     exit 1
